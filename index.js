e7c62f4c4e663252fc1173237c76c5cd53e6f613
rahman <rahmannurhayadi@gmail.com>
2023-07-31 11:04:07 +0700

    menambah fitur forgot dan reset password

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: true })) 
 
 const users = [{
     username : "bts",
