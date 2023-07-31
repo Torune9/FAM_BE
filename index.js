da2af07f00c954bc5e5ec08fa3f9469bc83d9d28
rahman <rahmannurhayadi@gmail.com>
2023-07-31 10:50:25 +0700

    menambah fitur forgot-password dan reset-password

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const users = [{
+    username : "bts",
+    password : 123,
+    email : "bts@gmail.com",
+}]
+
+
+app.post('/api/login',(req,res)=>{
+        users.push(req.body)
+        res.send(users)
+})
+
+app.get('/api/user',(req,res)=>{
+    res.send(users)
+})
 
 app.post('/api/authentication/login',(req,res)=>{ 
 
-    const datas = [{
-        username : req.query.username,
-        password : req.query.password,
-        email : req.query.email
-    }]
-    
-    const [user] = datas
-    const authUser = datas.find(data => data.username == user.username && data.password == user.password)
+    const {username,password} = req.body
+    const authUser = users.find(data => data.username == username && data.password == password)
         if(authUser){
             res.json({
                 "message" : "Login berhasil"
@@ -20,6 +34,36 @@ app.post('/api/authentication/login',(req,res)=>{
                 "message" : "Gagal Login"
             })
         }
+})
+
+app.post('/api/forgot',(req,res)=>{
+    const {email} = req.body
+    const user =users.find((user)=>user.email == email)
+    if(user){
+        res.json({
+            "message" : `email reset password tlah di kirim ke email ${email}`
+        })
+    }else{
+        res.json({
+            "message" : `email tidak ditemukan ${email}`
+        })
+    }
+})
+
+app.post('/api/reset',(req,res)=>{
+    const {email,newPassword} = req.body
+    const user = users.find((user)=>user.email == email)
+    if(user){
+        user.password = newPassword
+        res.json({
+            "message" : `Password berhasil di reset`,
+    
+        })
+    }else{
+        res.json({
+            "message" : `email tidak ditemukan ${email}`
+        })
+    }
 })
 
 app.listen(3000,()=>{
