9e6d11fea6193d8b7376a2e1e4f9c87173173088
rahman <rahmannurhayadi@gmail.com>
2023-09-06 10:53:06 +0700

    feat:allow cors on api

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const routerAsset = require('./routes/asset')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.set('view engine','ejs')
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
 app.use(router)
 app.use(routerAsset)
+
 app.listen(3000,()=>{
     console.log("Running In Port 3000");
 })
