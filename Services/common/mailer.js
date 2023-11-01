9179b13c0ba0d34f0730b9bfc8e037f8c39e5e02
rahman <rahmannurhayadi@gmail.com>
2023-11-01 11:05:52 +0700

    refactor:change service email

--- Services/common/mailer.js
+++ Services/common/mailer.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 // "use strict";
 
 const transporter = nodemailer.createTransport({
-  // host: "sandbox.smtp.mailtrap.io",
-  // port: 587,
-  // secure : false,
-  service :'gmail',
+  host: "sandbox.smtp.mailtrap.io",
+  port: 587,
+  secure : false,
+  // service :'gmail',
   auth: {
-    user: process.env.GMAIL_USERNAME,
-    pass : process.env.GMAIL_PASSWORD
+    user: process.env.EMAIL_USERNAME,
+    pass : process.env.EMAIL_PASSWORD
   }
 });
 
