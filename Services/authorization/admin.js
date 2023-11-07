dc1f98077bf081d72d7e6066bea2b41a0bbe4a3b
rahman <rahmannurhayadi@gmail.com>
2023-11-07 14:12:25 +0700

    fix admin auth

---
+++ Services/authorization/admin.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+
+const authenticateAdmin = (req, res, next) => {
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    if (decoded.role !== 'ADMIN') {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+    req.user = decoded; 
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+
+module.exports = { authenticateAdmin };
