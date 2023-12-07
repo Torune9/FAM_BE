bada0b23b3c891081047c301c5dfe62628162b4a
rahman <rahmannurhayadi@gmail.com>
2023-12-07 09:44:18 +0700

    feat : adding deleteMdAsset

---
+++ Controllers/Assets/master_assets/deleteMdAsset.js
@@ -0,0 +1,29 @@
+const {MD_Asset} = require('../../../models')
+
+
+const DeleteSoftMdAsset = async (req,res) => {
+    try {
+        const {id} = req.params
+        const mdAsset = await MD_Asset.findOne({
+            where : {
+                id : id
+            }
+        })
+
+       if (!mdAsset) {
+           res.json({
+               message : `${id} not found`
+           })
+        }else{
+            mdAsset.is_deleted = true
+            await mdAsset.save()
+           res.json({
+            message : 'Soft Deleted'
+           })
+       }
+    } catch (error) {
+        res.send(error.message)
+    }
+}
+
+module.exports = DeleteSoftMdAsset
