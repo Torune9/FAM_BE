1d04581ac9f08c7bccea5fb985bf86a1d037ccf2
rahman <rahmannurhayadi@gmail.com>
2023-12-07 09:44:44 +0700

    feat(master_asset controllers) : adding restoreMdAsset

---
+++ Controllers/Assets/master_assets/restoreMdAsset.js
@@ -0,0 +1,42 @@
+const {MD_Asset} = require('../../../models')
+const RestoreMdAsset = async (req,res)=>{
+    try {
+        const {id} = req.params
+        const data = await MD_Asset.findOne(
+            {
+                where :{
+                    id : id
+                }
+            }
+        )
+        
+        if (data) {
+            await Asset.update(
+                {is_deleted : false},
+                {
+                    where : {
+                        id : id
+                        }
+                    },
+                )
+            res.json({
+                status : 'Ok',
+                message : `Has been restore`
+               })
+        }else{
+            res.status(404)
+            res.json({
+                message : `Failed to restore,Code: ${id} not found!`,
+                code : res.statusCode,
+            })
+          
+       }
+        
+    } catch (error) {
+        res.json({
+            eror : error
+        })
+    }
+}
+
+module.exports = RestoreMdAsset
