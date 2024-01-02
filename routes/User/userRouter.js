49c8e6a7f8699ae4eb060e2697c8e6dceb5ed876
rahman <rahmannurhayadi@gmail.com>
2024-01-02 11:53:33 +0700

    feat : adding resetSendlink route

--- routes/User/userRouter.js
+++ routes/User/userRouter.js
@@ -2,14 +2,17 @@ const userRouter = require('express').Router()
 const {upload,filter} = require('../../Services/utils/imageUploads')
 
 const ForgotContoller = require('../../Controllers/Action/ForgotController')
-const ResetController = require('../../Controllers/Action/ResetController')
-const GetUserController = require('../../Controllers/Action/GetUserController')
+const {ResetController,sendLink} = require('../../Controllers/Action/ResetController')
+const {GetUserController,GetUsernameById} = require('../../Controllers/Action/GetUserController')
 const updateUserController = require('../../Controllers/User/UpdateUserController')
 const DeleteController = require('../../Controllers/User/DeleteUserController')
-const{ UploadImageUser,getImage} = require('../../Controllers/User/UserImageController')
+const{ manageUser,getImage} = require('../../Controllers/User/UserImageController')
 
-userRouter.get('/api/listUser',GetUserController.GetUserController)
+userRouter.get('/api/listUser',GetUserController)
+
 userRouter.get('/api/image/:id',getImage)
+
+userRouter.get('/api/username/:id',GetUsernameById)
 
 userRouter.put('/api/update/:id',updateUserController.updateUserController)
 
@@ -17,9 +20,11 @@ userRouter.delete('/api/delete',DeleteController.DeleteController)
 
 userRouter.post('/api/forgot',ForgotContoller.ForgotController)
 
-userRouter.post ('/api/image/:id/upload',upload.single('image'),UploadImageUser)
+userRouter.post ('/api/image/:id/upload',upload.single('image'),manageUser)
 userRouter.use(filter)
 
-userRouter.put('/api/reset-password/:token',ResetController.ResetController)
+userRouter.post('/api/new-password/:token',ResetController)
+
+userRouter.get('/user/newPassword/:token',sendLink)
 
 module.exports = userRouter
