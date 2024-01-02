1e0d2703a60ec7da40dcfd66316d83cfb98430ab
rahman <rahmannurhayadi@gmail.com>
2024-01-02 13:52:12 +0700

    fix : fix cors

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.set('view engine','ejs')
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/resources',staticFile);
-app.use((req, res, next) => {
-    res.status(404).render('notFound');
-});
 
 app.use(cors())
 app.use(routes)
 
+app.use((req, res, next) => {
+    res.status(404).render('notFound');
+});
 app.listen(3000,()=>{
     console.log("Running In Port 3000");
 })
