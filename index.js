0f867dcdea809339481e5d6c5e753589c92a0563
rahman <rahmannurhayadi@gmail.com>
2024-01-02 11:42:34 +0700

    feat : adding not found routes

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.set('view engine','ejs')
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/resources',staticFile);
+app.use((req, res, next) => {
+    res.status(404).render('notFound');
+});
 
 app.use(cors())
 app.use(routes)
