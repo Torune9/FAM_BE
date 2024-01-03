394f4656eb3da79c2c8956355b2dc4085b63f159
rahman <rahmannurhayadi@gmail.com>
2024-01-03 11:50:59 +0700

    fix  : fix getting data from status category

--- Controllers/inspect/category/getStatusCategory.js
+++ Controllers/inspect/category/getStatusCategory.js
@@ -2,7 +2,7 @@ const {status_category} = require('../../../models')
 const getStatusCategory = async (req,res)=>{
     try {
         const status = await status_category.findAll({
-            attributes : ['status','createdAt']
+            attributes : ['status','status_code','createdAt']
         })
         res.json({
             message : 'Ok',
