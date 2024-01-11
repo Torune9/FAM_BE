71bb19ed946e57d81b938818c12861f08bb5c5ed
rahman <rahmannurhayadi@gmail.com>
2024-01-11 14:59:30 +0700

    fix : delete route create history

--- routes/Inspection/inspection.js
+++ routes/Inspection/inspection.js
@@ -1,13 +1,7 @@
 const inspectRouter = require('express').Router()
 
-const createInspect = require('../../Controllers/inspect/history/createInspection')
 const getInspection = require('../../Controllers/inspect/history/getInspection')
 
-const inspectorAcc = require('../../Services/authorization/inspector')
-const {filter,upload} = require ('../../Services/utils/imageUploads')
-
-inspectRouter.post('/asset/:id/inspection',inspectorAcc,upload.array('images'),createInspect)
 inspectRouter.get('/asset/history/',getInspection)
-inspectRouter.use(filter)
 
 module.exports = inspectRouter
