b84eda899a16e3af4c7ab7556509a975d204c329
rahman <rahmannurhayadi@gmail.com>
2024-01-16 09:40:03 +0700

    feat : add database transaction

--- Controllers/inspect/attachment/createInspection.js
+++ Controllers/inspect/attachment/createInspection.js
@@ -1,55 +1,23 @@
-const { Attachment, Asset, History, Sequelize } = require("../../../models");
+const { Attachment, Asset, History, Sequelize, sequelize } = require("../../../models");
 
 const createAttachment = async (req, res) => {
-  const { inspector, information, status,statusCode } = req.body;
-  const files = req.files;
-  const { id, code } = req.params;
-  const date = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
-  const filename = files.map((file) => {
-    return file.filename;
-  });
-  const data = {
-    asset_code: code,
-    attachmentId: id,
-    inspector: inspector,
-    information: information,
-    attachments: filename,
-    status : status
-  };
-  try {
-    const assets = await Asset.findOne({
-      where: {
-        id: id,
-      },
+    const { inspector, information, status,statusCode } = req.body;
+    const files = req.files;
+    const { id, code } = req.params;
+    const date = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000);
+    const filename = files.map((file) => {
+      return file.filename;
     });
-    const existingInspection = await History.findOne({
-      where: {
-        attachmentId: id,
-        inspection_date: {
-          [Sequelize.Op.gt]: new Date(),
-        },
-      },
-    });
-    if (!assets) {
-      return res.status(404).json({
-        message: "Asset not found",
-      });
-    }
-
-    if (!information || !inspector || files.length < 0) {
-      return res.status(406).json({
-        message: `field can't be empty`,
-      });
-    }
-    if (existingInspection) {
-      return res
-        .status(408)
-        .json({
-          message: "The inspection has been carried out, please wait one week",
-        });
-    } else {
-      await Attachment.create(data);
-      await History.create({
+    const data = {
+      asset_code: code,
+      attachmentId: id,
+      inspector: inspector,
+      information: information,
+      attachments: filename,
+      status : status
+    };
+    const dataHistory = (assets)=>{
+      return {
         name : assets.name,
         asset_code: assets.asset_code,
         status: status,
@@ -57,16 +25,58 @@ const createAttachment = async (req, res) => {
         statusCode : statusCode,
         attachmentId: id,
         information : information
-      });
-      res.json({
-        message: "Inspection has been created",
-        file: files,
-      });
+      }
     }
-  } catch (error) {
-    res.send(error);
-    console.log(error);
-  }
-};
+    try {
+      await sequelize.transaction(async (t)=>{
+        const assets = await Asset.findOne({
+          where: {
+            id: id,
+          },
+        });
+        const existingInspection = await History.findOne({
+          where: {
+            attachmentId: id,
+            inspection_date: {
+              [Sequelize.Op.gt]: new Date(),
+            },
+          },
+        });
+        if (!assets) {
+          return res.status(404).json({
+            message: "Asset not found",
+          });
+        }
+  
+        if (!information || !inspector || files.length < 0) {
+          return res.status(406).json({
+            message: `field can't be empty`,
+          });
+        }
+        if (existingInspection) {
+          return res
+            .status(408)
+            .json({
+              message: "The inspection has been carried out, please wait one week",
+            });
+        } else {
+          await Attachment.create(data,{
+            transaction : t
+          });
+          await History.create(dataHistory(assets),{
+            transaction : t
+          });
+          res.json({
+            message: "Inspection has been created",
+            file: files,
+          });
+        }
+      })
+      
+    } catch (error) {
+      res.send(error);
+      console.log(error);
+    }
+  };
 
 module.exports = createAttachment;
