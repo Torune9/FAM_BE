4b58d4c91be5ac50714d85217e9f30d5dd18b2f2
rahman <rahmannurhayadi@gmail.com>
2024-01-18 11:22:46 +0700

    fix : fix authoriztion for inspection attchment

--- routes/Inspection/attachments.js
+++ routes/Inspection/attachments.js
@@ -3,10 +3,10 @@ const attachRouter = require('express').Router()
 const createAttachment = require('../../Controllers/inspect/attachment/createInspection')
 const getAttachmentInspect = require('../../Controllers/inspect/attachment/getAttachmentInspect')
 
-const auditorAcc = require('../../Services/authorization/auditor')
+const inspectorAcc = require('../../Services/authorization/inspector')
 const {filters,uploads} = require('../../Services/utils/attachmentsUploads')
 
-attachRouter.post('/asset/attachment/:id/:code',auditorAcc,uploads.array('files'),createAttachment)
+attachRouter.post('/asset/attachment/:id/:code',inspectorAcc,uploads.array('files'),createAttachment)
 attachRouter.get('/asset/attachment/:id',getAttachmentInspect)
 attachRouter.use(filters)
 
