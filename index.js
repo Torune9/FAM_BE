0664490eabd172ec69ac72950e3eec0f00fd4721
rahman <rahmannurhayadi@gmail.com>
2024-08-30 08:05:36 +0700

    fix : deleye image upload

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use('/resources',staticFile);
 
 app.use(cors())
 app.use(routes)
-
 app.use((req, res, next) => {
     res.status(404).render('notFound');
 });
+
 app.listen(3000,()=>{
     console.log("Running In Port 3000");
 })
